fef4edac38ffd70951f02df80e5edcfab046834a
scampos37 <scampos37@ucmerced.edu>
2023-04-26 19:51:21 -0700

    addition of label

---
+++ labelRecognizer/perspectiveTesting/perspective.cpp
@@ -0,0 +1,185 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+void edgeMap(cv::Mat bw, cv::Mat edgeImage) {
+    /* Get the edge map using the canny operator */
+    cv::Canny(bw, edgeImage, 100, 100, 3, true);
+}
+
+void blur(cv::Mat inputImage, cv::Mat outputImage) {
+    cv::Size size(3, 3);
+    cv::GaussianBlur(inputImage, outputImage, size, 3);
+}
+
+std::vector<cv::Vec4i>detectLines(cv::Mat bw) {
+    std::vector<cv::Vec4i> lines;
+    //cv::HoughLinesP(bw, lines, 1, CV_PI/180, 70, 30, 10);
+    cv::HoughLinesP(bw, lines, 1, CV_PI/180, 150, 90, 20);
+    return lines;
+}
+
+std::vector<cv::Vec4i>detectOuterLines(cv::Mat bw) {
+    std::vector<cv::Vec4i> lines;
+    cv::HoughLinesP(bw, lines, 1, CV_PI/180, 70, 100, 10);
+    return lines;
+}
+
+cv::Point2f computeIntersect(cv::Vec4i a, cv::Vec4i b)
+{
+    int x1 = a[0], y1 = a[1], x2 = a[2], y2 = a[3];
+    int x3 = b[0], y3 = b[1], x4 = b[2], y4 = b[3];
+
+    if (float d = ((float)(x1-x2) * (y3-y4)) - ((y1-y2) * (x3-x4)))
+    {
+        cv::Point2f pt;
+        pt.x = ((x1*y2 - y1*x2) * (x3-x4) - (x1-x2) * (x3*y4 - y3*x4)) / d;
+        pt.y = ((x1*y2 - y1*x2) * (y3-y4) - (y1-y2) * (x3*y4 - y3*x4)) / d;
+        return pt;
+    }
+    else
+        return cv::Point2f(-1, -1);
+}
+
+void sortCorners(std::vector<cv::Point2f>& corners, cv::Point2f center)
+{
+    std::vector<cv::Point2f> top, bot;
+
+    for (int i = 0; i < corners.size(); i++)
+    {
+        if (corners[i].y < center.y)
+            top.push_back(corners[i]);
+        else
+            bot.push_back(corners[i]);
+    }
+
+    cv::Point2f tl = top[0].x > top[1].x ? top[1] : top[0];
+    cv::Point2f tr = top[0].x > top[1].x ? top[0] : top[1];
+    cv::Point2f bl = bot[0].x > bot[1].x ? bot[1] : bot[0];
+    cv::Point2f br = bot[0].x > bot[1].x ? bot[0] : bot[1];
+
+    corners.clear();
+    corners.push_back(tl);
+    corners.push_back(tr);
+    corners.push_back(br);
+    corners.push_back(bl);
+}
+
+void drawLines(cv::Mat image, std::vector<cv::Vec4i> lines) {
+    // Draw lines
+    for (int i = 0; i < lines.size(); i++) {
+        cv::Vec4i v = lines[i];
+        cv::line(image, cv::Point(v[0], v[1]), cv::Point(v[2], v[3]), CV_RGB(0,255,0));
+    }
+}
+
+int main(int argc, char** argv) {
+    if(argc != 2) {
+         std::cout <<" Usage: display_bw ImageToLoadAndDisplay" << std::endl;
+         return -1;
+    }
+
+    cv::Mat image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Read the file
+    cv::Mat bw;   // Read the file
+    cv::cvtColor(image, bw, CV_BGR2GRAY);
+
+    /* Check for valid input */
+    if(!bw.data) {
+        std::cout <<  "Could not open or find the bw" << std::endl;
+        return -1;
+    }
+
+
+    std::vector<cv::Vec4i> lines;
+
+    cv::Mat contourImage = bw.clone();
+    edgeMap(bw, bw);
+    std::vector<std::vector<cv::Point> > contours;
+
+//    blur(contourImage, contourImage);
+
+    cv::adaptiveThreshold(contourImage, contourImage, 255, CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY_INV, 11, 2);
+
+    edgeMap(contourImage, contourImage);
+    cv::findContours(contourImage, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+    std::cout << contours.size() << " contours" << std::endl;
+
+    double maxArea = 0;
+    std::vector<cv::Point> biggestContour;
+    std::vector<cv::Point> approx;
+    for (auto &contour: contours) {
+        double area = contourArea(contour);
+        double peri = cv::arcLength(contour, true);
+        cv::approxPolyDP(contour, approx, 0.02*peri, true);
+        if (area > maxArea && approx.size() == 4) { 
+            maxArea = area; 
+            biggestContour = approx; 
+        }
+    }
+
+    contourImage = image.clone();
+    contours.clear();
+    contours.push_back(biggestContour);
+    cv::drawContours(contourImage, contours, -1, CV_RGB(0,255,0));
+
+    cv::Mat contourLineImage = image.clone();
+    //lines = detectLines(contourImage);
+    drawLines(contourLineImage, lines);
+ 
+    lines = detectLines(bw);
+    std::cout << lines.size() << " lines" << std::endl;
+    
+    cv::Mat lineImage = image.clone();
+    drawLines(lineImage, lines);
+   
+    if (biggestContour.size() != 4)
+    {
+        std::cout << "The object is not quadrilateral!" << std::endl;
+        return -1;
+    }
+
+    std::vector<cv::Point2f> corners;
+    cv::Mat(biggestContour).convertTo(corners, cv::Mat(corners).type());
+
+    // Get mass center
+    cv::Point2f center(0,0);
+    for (int i = 0; i < corners.size(); i++)
+        center += corners[i];
+
+    center *= (1. / corners.size());
+    sortCorners(corners, center);
+
+    // Define the destination image
+    cv::Rect boundingRect = cv::boundingRect(biggestContour);
+    cv::Mat quad = cv::Mat::zeros(boundingRect.size(), CV_8UC3);
+
+    // Corners of the destination image
+    std::vector<cv::Point2f> quad_pts;
+    quad_pts.push_back(cv::Point2f(0, 0));
+    quad_pts.push_back(cv::Point2f(quad.cols, 0));
+    quad_pts.push_back(cv::Point2f(quad.cols, quad.rows));
+    quad_pts.push_back(cv::Point2f(0, quad.rows));
+
+    // Get transformation matrix
+    cv::Mat transmtx = cv::getPerspectiveTransform(corners, quad_pts);
+
+    // Apply perspective transformation
+    cv::warpPerspective(image, quad, transmtx, quad.size());
+    cv::imshow("quadrilateral", quad);
+
+//    cv::imshow("Original", image);
+    cv::imshow("Edge detected bw", bw);
+    cv::imshow("Lines found", lineImage);
+    cv::imshow("Contours found", contourImage);
+    cv::imshow("Lines found from contours", contourLineImage);
+
+    cv::imwrite("edge.jpg", bw);
+    cv::imwrite("lines.jpg", lineImage);
+    cv::imwrite("contours.jpg", contourImage);
+    cv::imwrite("contourLines.jpg", contourLineImage);
+
+    cv::waitKey(0);                   
+    return 0;
+}
+
---
+++ labelRecognizer/python/python/contours.py
@@ -0,0 +1,348 @@
+import cv2
+import sys
+import os
+import math
+import random
+import numpy as np
+from categories import easy_labels
+
+
+def get_random_color():
+    return (
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255)
+    )
+
+
+def corners(cnt):
+    """
+    Reorders 4 corners into clockwise order.
+    (starting with the top-left corner)
+    """
+    # Axis to look at depends on the number of dimensions of contour
+    a = len(cnt.shape)-1
+
+    rect = np.zeros(cnt.shape, dtype=cnt.dtype)
+
+    s = np.sum(cnt, axis=a)
+    rect[0] = cnt[np.argmin(s)]
+    rect[2] = cnt[np.argmax(s)]
+
+    diff = np.diff(cnt, axis=a)
+    rect[1] = cnt[np.argmin(diff)]
+    rect[3] = cnt[np.argmax(diff)]
+
+    return rect
+
+
+def filter_contours_size(contours, size_thresh):
+    """
+    Filters list of contours to contours that are at least
+    the size of the threshold passed (size is pixel area).
+    """
+    filtered_contours = []
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > size_thresh:
+            filtered_contours.append(cnt)
+
+    print('%d/%d large contours found' % (
+        len(filtered_contours), len(contours)))
+    return filtered_contours
+
+
+def filter_contours_centroid(contours, distance_thresh, centroid):
+    """
+    Calculate Euclidean distances of contour centroids,
+    only keeping contours with distances less than the
+    second passed parameter.
+    """
+    filtered_contours = []
+    cx_im, cy_im = centroid
+    for cnt in contours:
+        # Moments of the contour
+        M = cv2.moments(cnt)
+
+        # Find the centroid
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+
+        # Euclidean distance from center
+        dist = math.sqrt((cx - cx_im)**2 + (cy - cy_im)**2)
+
+        if dist < distance_thresh:
+            filtered_contours.append(cnt)
+
+    print('%d/%d centroid contours found' % (
+        len(filtered_contours), len(contours)))
+    return filtered_contours
+
+
+def filter_contours_center(contours, centroid):
+    """
+    Eliminate all contours that do not contain the center of the image.
+    """
+    filtered_contours = []
+    # Find contour in the middle of the image
+    for cnt in contours:
+        # Check if contour contains the center of the image
+        if cv2.pointPolygonTest(cnt, centroid, False) > 0:
+            filtered_contours.append(cnt)
+
+    print('%d/%d center contours found' % (
+        len(filtered_contours), len(contours)))
+    return filtered_contours
+
+
+def draw_image(image, name):
+    """
+    Draws the Mat image on a frame with name 'name,
+    waits for a keypress, then closes the window.'
+    """
+    cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+    cv2.startWindowThread()
+    cv2.resizeWindow(name, 1500, 2300)
+    cv2.imshow(name, image)
+    cv2.waitKey()
+    cv2.destroyWindow(name)
+
+
+def invert_color(image):
+    inverted = 255 - image
+    return inverted
+
+
+# Most images seem to be approximately 5,000,000 pixels large.
+def downscale_im(im, threshold=2000000):
+    """
+    Recursively resize image until its size is less than the provided
+    threshold.
+
+    Returns the resized image.
+    """
+    im_x = len(im[0])
+    im_y = len(im)
+    im_size = im_x * im_y
+    if im_size > threshold:
+        return downscale_im(cv2.resize(im, (0, 0), fx=0.5, fy=0.5))
+
+    return im
+
+
+def adaptive_threshold(image):
+    return cv2.adaptiveThreshold(
+        image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY, 101, 5
+    )
+
+
+def approx_rect(contours, epsilon=50):
+    """
+    Iteratively apply approximations to a contour to make it a rectangle.
+
+    If no rectangular approximation is found, return False.
+    """
+    label_contour = None
+    label_corners = None
+    for i in range(2):
+        for c in contours:
+            contour_corners = cv2.approxPolyDP(c, epsilon, True)
+            if len(contour_corners) == 4:
+                label_corners = contour_corners
+                label_contour = c
+                break
+        epsilon *= 2
+
+    return label_contour, label_corners
+
+
+def get_angle(orientation):
+    # Taken from http://stackoverflow.com/questions/
+    # 22045882/modify-or-delete-exif-tag-orientation-in-python
+    # print('Orientation: %s' % orientation)
+    if orientation == 6:
+        return -90
+    elif orientation == 8:
+        return 90
+    elif orientation == 3:
+        return 180
+
+    # Doesn't handle transposed images
+    return 0
+
+
+def rotate_image(image, angle):
+    # print("Rotate %s" % angle)
+    image_center = tuple(np.array(image.shape[0:2])/2)
+    rot_mat = cv2.getRotationMatrix2D(
+        image_center, angle, 1.0)
+    result = cv2.warpAffine(
+        image, rot_mat, image.shape[0:2], flags=cv2.INTER_LINEAR)
+    return result
+
+
+# Adapted from here:
+# http://stackoverflow.com/questions/16265673/rotate-image-by-90-180-or-270-degrees # noqa
+def rotate_image_90n(image, angle=0):
+    # print("Rotate %s" % angle)
+    n = ((angle/90) % 4) * 90
+
+    flip_h_or_v = 1
+    if angle > 0:
+        flip_h_or_v = 0
+
+    n = np.abs(int(angle/90))
+    for i in range(n):
+        image = cv2.transpose(image, image)
+        image = cv2.flip(image, flip_h_or_v)
+
+    return image
+
+
+def contour(imagepath, invert=False, demo=False, orientation=1):
+    """
+    Finds a label contour in the specified image.
+    Returns an image with just the extracted label, or False if
+    no label is found.
+    """
+
+    if not os.path.exists(imagepath):
+        print("Could not find %s" % imagepath)
+        return False
+
+    fileName = os.path.basename(imagepath)
+
+    filenames = {}
+    image_types = ["gray", "blur", "thresholdg", "thresholdm",
+                   "contour", "original", "hist", "final"]
+    for it in image_types:
+        dir_path = it.upper()
+        if demo:
+            dir_path = 'demo'
+        filenames[it] = os.path.join(dir_path, it.lower()) + fileName
+
+    im = cv2.imread(imagepath)
+    im = downscale_im(im)
+    # Account for rotated images
+    angle = get_angle(orientation)
+    if angle != 0:
+        im = rotate_image_90n(im, angle)
+
+    cv2.imwrite(filenames['original'], im)
+    im_x = len(im[0])
+    im_y = len(im)
+    im_size = im_x * im_y
+
+    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    if invert:
+        imgray = invert_color(imgray)
+
+    imageToBeCut = imgray
+    cv2.imwrite(filenames['gray'], imgray)
+    imgray = cv2.medianBlur(imgray, 3)
+    cv2.imwrite(filenames['blur'], imgray)
+    cv2.equalizeHist(imgray, imgray)
+    cv2.imwrite(filenames['hist'], imgray)
+
+    imgray = cv2.bilateralFilter(imgray, 5, 20, 50)
+    cv2.imwrite(filenames['thresholdm'], imgray)
+    thresh = adaptive_threshold(imgray)
+
+    cv2.imwrite(filenames['thresholdg'], thresh)
+    contours, hierarchy = cv2.findContours(
+        thresh.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE
+    )
+
+    # Filter based on contour size
+    large_contours = filter_contours_size(contours, im_size/10)
+
+    # Now filter based on the location of the contour's centroid
+    centroid = (int(im_x/2), int(im_y/2))
+
+#    for x, j in enumerate(large_contours):
+#        name = filenames['contour'].replace('.jpg', '-' + str(x) + '.jpg')
+#        cv2.drawContours(im, [j], -1, get_random_color(), -5)
+#        cv2.circle(im, centroid, 20, (0, 0, 0), thickness=-1)
+#        cv2.imwrite(name, im)
+#        im = cv2.imread(imagepath)
+
+    # Filter to contours containing central point
+    label_contours = filter_contours_center(large_contours, centroid)
+
+    if len(label_contours) == 0:
+        return False
+
+#    name = filenames['contour']
+#    cv2.drawContours(im, [label_contours[0]], -1, get_random_color(), -5)
+#    cv2.circle(im, centroid, 20, (0, 0, 0), thickness=-1)
+#    cv2.imwrite(name, im)
+
+    label_contour, label_corners = approx_rect(label_contours)
+
+    if label_contour is None:
+        print('No rectangular contour found')
+        return False
+
+    # Arrange corners in clockwise order
+    label_corners = corners(label_corners)
+
+    # Draw contour on original image
+    cv2.drawContours(im, [label_contour], -1, get_random_color(), -5)
+    cv2.imwrite(filenames['contour'], im)
+
+    # Find minimum containing (rotated) rectangle
+    # to set up transformation image
+    min_rect = cv2.minAreaRect(label_contour)
+    size = (int(min_rect[1][0]), int(min_rect[1][1]))
+    angle = min_rect[2]
+
+    # Account for rotated images
+    if abs(angle) > 45:
+        angle = angle % 45
+        size = (size[1], size[0])
+
+    np_corners = np.array(
+        [[0, 0], [size[0], 0], [size[0], size[1]], [0, size[1]]],
+        np.float32
+    )
+    new_corners = corners(np_corners)
+#    tps = cv2.createThinPlateSplineShapeTransformer(25000)
+#    tps.estimateTransformation(label_contour, new_corners
+    M = cv2.getPerspectiveTransform(
+        np.array(label_corners, np.float32),
+        new_corners
+    )
+    label_im = cv2.warpPerspective(imageToBeCut, M, dsize=size)
+
+    thresh = adaptive_threshold(label_im)
+    cv2.equalizeHist(thresh, thresh)
+    cv2.imwrite(filenames['final'], thresh)
+    return thresh
+
+
+def process_all_easy(dirpath):
+    """
+    Convenience method for running the contour function on all
+    jpg files in a directory.
+    """
+    labels = easy_labels()
+    for label in labels:
+        print(label.name)
+        contour(os.path.join(dirpath, label.name + '.jpg'))
+
+
+def main():
+    pass
+
+# Run when called from command line, not import.
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+        if os.path.isdir(arg):
+            process_all_easy(arg)
+        elif os.path.isfile(arg):
+            contour(arg)
+        else:
+            print('Usage:\n\t\'python3 contours.py <path>\'\n\
+                    (path should be a file or directory).')
