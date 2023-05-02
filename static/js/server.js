d2cee73df08cd4b9a2a3ed82c92878f227a6353f
acardenas39 <114961944+acardenas39@users.noreply.github.com>
2023-05-01 23:04:39 -0700

    Table now displays output from model

--- static/js/server.js
+++ static/js/server.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 const port = 8000;
 const filePath = '../../LabelExtractionModel/Results/labelList.json';
+const filePath2 = '../../labelRecognizer/python/ocr_results.json';
 
 const server = http.createServer((req, res) => {
   if (req.url === '/labelList.json' && req.method === 'GET') {
@@ -15,9 +16,47 @@ const server = http.createServer((req, res) => {
         res.end(data);
       }
     });
+  } else if (req.url === '/ocr_results.json' && req.method === 'GET') {
+    fs.readFile(filePath2, (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end('Internal server error');
+      } else {
+        try {
+          const jsonArray = JSON.parse(data.toString());
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(jsonArray));
+        } catch (error) {
+          res.statusCode = 500;
+          res.end('Error parsing JSON data');
+        }
+      }
+    });
+  } else if (req.url === '/ocr_results.json' && req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      try {
+        const jsonArray = JSON.parse(body);
+        fs.writeFile(filePath2, JSON.stringify(jsonArray), err => {
+          if (err) {
+            res.statusCode = 500;
+            res.end('Internal server error');
+          } else {
+            res.statusCode = 200;
+            res.end('JSON data updated successfully');
+          }
+        });
+      } catch (error) {
+        res.statusCode = 500;
+        res.end('Error parsing JSON data');
+      }
+    });
   } else {
-    res.statusCode = 404;
-    res.end('Not found');
+      res.statusCode = 404;
+      res.end('Not found');
   }
 });
 
